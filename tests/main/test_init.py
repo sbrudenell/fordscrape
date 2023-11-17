46118badbf54bf47f2fe33dc2b10e4fbc34a7495
Steven Brudenell <steven.brudenell@gmail.com>
2023-11-17 14:38:41 -0900

    test fordscrape init --param=key=value

--- tests/main/test_init.py
+++ tests/main/test_init.py
@@ -21,3 +21,19 @@ def test_minimum_args(new_state_path: pathlib.Path) -> None:
     state = State(new_state_path)
     params = state.get_search_params()
     assert params["model"] == "testmodel"
+
+
+def test_many_args(new_state_path: pathlib.Path) -> None:
+    result = shell(
+        f"fordscrape init {new_state_path} --model=testmodel "
+        "--make=testmake --market=testmarket --param=engine=testengine "
+        "--param=modeltrim=testmodeltrim"
+    )
+    assert result.exit_code == 0
+    state = State(new_state_path)
+    params = state.get_search_params()
+    assert params["model"] == "testmodel"
+    assert params["make"] == "testmake"
+    assert params["market"] == "testmarket"
+    assert params["engine"] == "testengine"
+    assert params["modeltrim"] == "testmodeltrim"
